aa3c136c2d0489c182d456997b7aa6f75bea5e17
YasodhaHeshan <yasodhaheshan2002@gmail.com>
2024-11-07 18:29:23 +0530

    update RegisterActivity.java , add DatabaseHelper.java, UserController.java

--- app/src/main/java/com/example/mad_project/RegisterActivity.java
+++ app/src/main/java/com/example/mad_project/RegisterActivity.java
@@ -3,8 +3,12 @@ package com.example.mad_project;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mad_project.data.DatabaseHelper;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -16,15 +20,32 @@ public class RegisterActivity extends AppCompatActivity {
         Button login = findViewById(R.id.btnLogin);
         Button customer = findViewById(R.id.btnCustomer);
         Button driver = findViewById(R.id.btnDriver);
-        Button owner = findViewById(R.id.btnCustomer);
-        Button Signup = findViewById(R.id.btnSignup);
+        Button owner = findViewById(R.id.btnOwner);
+        Button signup = findViewById(R.id.btnSignup);
+
+
+        EditText firstName = findViewById(R.id.editTextText2);
+        EditText lastName = findViewById(R.id.editTextText4);
+        EditText email = findViewById(R.id.editTextText5);
+        EditText password = findViewById(R.id.editTextText7);
+        EditText mobile = findViewById(R.id.editTextText6);
 
         login.setOnClickListener(v -> {
             Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
             startActivity(intent);
         });
 
-        Signup.setOnClickListener(v -> {
+        signup.setOnClickListener(v -> {
+            String firstNameStr = firstName.getText().toString();
+            String lastNameStr = lastName.getText().toString();
+            String emailStr = email.getText().toString();
+            String passwordStr = password.getText().toString();
+            String mobileStr = mobile.getText().toString();
+
+            DatabaseHelper dbHelper = new DatabaseHelper(this);
+            dbHelper.addUser(firstNameStr, lastNameStr, emailStr, passwordStr, mobileStr);
+
+            Toast.makeText(this, "User registered successfully", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(RegisterActivity.this, PaymentActivity.class);
             startActivity(intent);
         });
