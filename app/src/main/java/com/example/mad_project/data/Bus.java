75ffcf2d74d99ee204894b2c6caa6ed56f8486ea
AdithyaSean <adithyasean@gmail.com>
2024-11-07 17:52:19 +0530

    Created: Bus.java Created: User.java

--- app/src/main/java/com/example/mad_project/data/Bus.java
+++ app/src/main/java/com/example/mad_project/data/Bus.java
@@ -1,4 +1,67 @@
 package com.example.mad_project.data;
 
 public class Bus {
+    private int id;
+    private String departureLocation;
+    private String arrivalLocation;
+    private String departureTime;
+    private String arrivalTime;
+    private int availableSeats;
+
+    public Bus(int id, String departureLocation, String arrivalLocation, String departureTime, String arrivalTime, int availableSeats) {
+        this.id = id;
+        this.departureLocation = departureLocation;
+        this.arrivalLocation = arrivalLocation;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+        this.availableSeats = availableSeats;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDepartureLocation() {
+        return departureLocation;
+    }
+
+    public void setDepartureLocation(String departureLocation) {
+        this.departureLocation = departureLocation;
+    }
+
+    public String getArrivalLocation() {
+        return arrivalLocation;
+    }
+
+    public void setArrivalLocation(String arrivalLocation) {
+        this.arrivalLocation = arrivalLocation;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(String arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public void setAvailableSeats(int availableSeats) {
+        this.availableSeats = availableSeats;
+    }
 }
