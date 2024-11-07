3dff155856f3cec91a72bd7a7f71f7a0f69d8b0d
JalithChamikara <premjalith@gmail.com>
2024-11-07 08:36:04 +0530

    Add scroll function to the dashboard and change navbar hover color

--- app/src/main/java/com/example/mad_project/MainActivity.java
+++ app/src/main/java/com/example/mad_project/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_select_bus);
+        setContentView(R.layout.activity_dashboard);
 
         EditText from = findViewById(R.id.fromField);
         EditText to = findViewById(R.id.toField);
