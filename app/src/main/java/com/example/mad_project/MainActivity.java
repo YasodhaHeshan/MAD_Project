e891cbaa7d41197b637e8837ccc2ca0f42dbce76
AdithyaSean <adithyasean@gmail.com>
2024-11-07 16:39:52 +0530

    MainActivity.java fix

--- app/src/main/java/com/example/mad_project/MainActivity.java
+++ app/src/main/java/com/example/mad_project/MainActivity.java
@@ -17,27 +17,11 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.map), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
-
-
-//        super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
-//        setContentView(R.layout.activity_main);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layoutmain), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
-//
-       Button registerbtn = findViewById(R.id.btnStart);
+        Button registerbtn = findViewById(R.id.btnStart);
 
         registerbtn.setOnClickListener(v -> {
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-          startActivity(intent);
-      });
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
     }
 }
