c4447fe00a88d9bbd37d32f8014f46d5bd79bb73
Adithya Ekanayaka <130180324+AdithyaSean@users.noreply.github.com>
2024-11-09 08:59:52 +0530

    fixed the database problem (#18)
    * MainActivity.java fix
    
    * Created: Bus.java
    Created: User.java
    
    * ORM database control and main classes and controllers
    
    * fixed the database problem

---
+++ app/src/main/java/com/example/mad_project/data/BookingDao.java
@@ -0,0 +1,15 @@
+package com.example.mad_project.data;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import java.util.List;
+
+@Dao
+public interface BookingDao {
+    @Insert
+    void insert(Booking booking);
+
+    @Query("SELECT * FROM Booking WHERE userId = :userId")
+    List<Booking> getBookingsByUserId(int userId);
+}
