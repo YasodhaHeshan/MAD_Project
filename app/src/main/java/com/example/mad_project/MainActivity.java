f06f05fbb3ae1001cadd6632cde1ca9cfd8d85f9
Senith Nimsara <124909394+senith02@users.noreply.github.com>
2024-11-11 06:17:24 +0530

    Create Booked Seats set for Store Booked Seats. (#21)
    * Create Booked Seats Set for store booked seats.

--- app/src/main/java/com/example/mad_project/MainActivity.java
+++ app/src/main/java/com/example/mad_project/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.mad_project;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
