8675b36f43c4a975a24e14acec327f0f90b3bbf8
YasodhaHeshan <yasodhaheshan2002@gmail.com>
2024-11-11 21:09:51 +0530

    create ProfileActivity.java

---
+++ app/src/main/java/com/example/mad_project/ui/ProfileActivity.java
@@ -0,0 +1,13 @@
+package com.example.mad_project.ui;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.mad_project.R;
+
+public class ProfileActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_profile);
+        }
+}
