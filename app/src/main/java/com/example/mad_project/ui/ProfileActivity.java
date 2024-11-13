cdce681ebbe9863e8d14c2d938ec9c00e1ba22dd
YasodhaHeshan <yasodhaheshan2002@gmail.com>
2024-11-13 13:40:40 +0530

    create update profile flow

--- app/src/main/java/com/example/mad_project/ui/ProfileActivity.java
+++ app/src/main/java/com/example/mad_project/ui/ProfileActivity.java
@@ -1,4 +1,24 @@
 package com.example.mad_project.ui;
 
-public class ProfileActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mad_project.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        Button btnUpdate = findViewById(R.id.btnUpdate);
+        btnUpdate.setOnClickListener(v -> {
+            Intent intent = new Intent(ProfileActivity.this, EditProfileActivity.class);
+            startActivity(intent);
+        });
+    }
 }
