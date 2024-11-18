ded969474696bb05bd525123eb9b89d7014e8898
AdithyaSean <adithyasean@gmail.com>
2024-11-18 23:44:51 +0530

    bus list view complete

--- app/src/main/java/com/example/mad_project/data/ScheduleDao.java
+++ app/src/main/java/com/example/mad_project/data/ScheduleDao.java
@@ -1,12 +1,8 @@
 package com.example.mad_project.data;
 
 import androidx.room.Dao;
-import androidx.room.Delete;
 import androidx.room.Insert;
-import androidx.room.Query;
 import androidx.room.Update;
-
-import java.util.List;
 
 @Dao
 public interface ScheduleDao {
